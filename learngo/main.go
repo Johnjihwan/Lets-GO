8d50b4971340b7964b5589cbfe86f5e42cdf9d1d
johnjihwan <s20062@gsm.hs.kr>
2021-02-19 01:18:46 +0900

    [UPDATE]What is different between small word to big word

---
+++ learngo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Johnjihwan/learngo/something"
+)
+
+func main() {
+	fmt.Println("Hello GO")
+	something.SayHello()
+}
---
+++ learngo/something/something.go
@@ -0,0 +1,11 @@
+package something
+
+import "fmt"
+
+// private function
+func sayBye() {
+	fmt.Println("Bye")
+}
+func SayHello() {
+	fmt.Println("Hello")
+}
